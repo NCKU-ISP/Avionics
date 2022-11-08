505414c93da78c57d751fd7e18e5888cd6499bd8
William Chang <wilicw1346@gmail.com>
2022-11-08 23:26:12 +0800

    feat: don't disable serial when enable debug

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,9 @@ void setup()
     delay(3000);
     Serial.begin(115200);
     // Serial.setDebugOutput(true);
+#if (!defined(USE_SERIAL_COMMS)) && (!defined(USE_SERIAL_DEBUGGER))
     Serial.end();
+#endif
 
     sys = new System();
     sys->init();
